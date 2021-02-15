d75861dec4889ce2fa127bee9adb4bca443737bd
Marcus Lima <marcus_fonseca@ymail.com>
2021-02-15 14:20:04 -0300

    Login with firebase auth

---
+++ app/src/pages/Login.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from "../config/firebase";
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  },
+};
+
+function Login() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Listen to the Firebase Auth state and set the local state.
+  useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      setIsSignedIn(!!user);
+      setIsLoading(false);
+    });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
+
+  if(isLoading) {
+    return (
+      <p>Loading...</p>
+    )
+  }
+
+  if (!isSignedIn) {
+    return (
+      <div>
+        <h1>Financee</h1>
+        <p>Please sign-in:</p>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      </div>
+    );
+  } 
+
+  return (
+    <div>
+      <h1>My App</h1>
+      <p>Welcome {firebase.auth()?.currentUser?.displayName}! You are now signed-in!</p>
+      <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+    </div>
+  );
+  
+}
+
+export default Login;
