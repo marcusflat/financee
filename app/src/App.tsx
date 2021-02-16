64d7456c87d9247d211299de8853eb6c25a68d89
Marcus Lima <marcus_fonseca@ymail.com>
2021-02-16 12:26:20 -0300

    install redux chrome extension

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Routes from "./routes/Routes";
-import firebase from './config/firebase';
 import { LoadingOverlay } from './components';
 import { useUserLogin } from './redux/user/hooks/useUseLogin';
 
--- app/src/redux/store.ts
+++ app/src/redux/store.ts
@@ -1,5 +1,5 @@
-import { create } from "domain";
 import { createStore, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import userSchema from "./user/userSchema";
 import userReducer from "./user/userReducer";
@@ -12,6 +12,10 @@ const reducers = combineReducers({
   user: userReducer
 })
 
-export const store = createStore(reducers, schema);
+export const store = createStore(
+  reducers, 
+  schema,
+  composeWithDevTools()
+  );
 
 export type RootState = ReturnType<typeof reducers>
